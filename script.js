e0f3c4c4562afb0bf3cc9a56c65acc53b0a198c1
vandana7024 <vandanadewangan1999@gmail.com>
2022-02-14 18:32:43 +0530

    Design Completed

---
+++ script.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        gray: "#8492A6",
+        myBlue: "#2F55D4",
+        darkblack: "#161C2D",
+        red: "#E43F52",
+        slate: "#f1f5f9",
+        pink: "#f87171",
+        yellow: "#fde047",
+        navyblue: "#202942",
+        slatemyblue: "#27314f",
+
+        sky: "#ADB5BD",
+      },
+    },
+  },
+};
